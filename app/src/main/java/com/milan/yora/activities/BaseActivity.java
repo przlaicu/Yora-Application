9873fddb285ce80d9b692febe392f3d9f22849bf
Milan <przlaicu@gmail.com>
2018-07-31 20:33:21 +0200

    integrating toolbar

--- app/src/main/java/com/milan/yora/activities/BaseActivity.java
+++ app/src/main/java/com/milan/yora/activities/BaseActivity.java
@@ -1,18 +1,33 @@
 package com.milan.yora.activities;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import com.milan.yora.R;
 import com.milan.yora.infrastructure.YoraApplication;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     protected YoraApplication application;
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedState){
         super.onCreate(savedState);
 
         application = (YoraApplication) getApplication();
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResId) {
+        super.setContentView(layoutResId);
+
+        toolbar = (Toolbar) findViewById(R.id.include_toolbar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
     }
 }
--- app/src/main/java/com/milan/yora/activities/MainActivity.java
+++ app/src/main/java/com/milan/yora/activities/MainActivity.java
@@ -2,10 +2,14 @@ package com.milan.yora.activities;
 
 import android.os.Bundle;
 
+import com.milan.yora.R;
+
 public class MainActivity extends BaseAuthenticatedActivity {
 
     @Override
     protected void onYoraCreate(Bundle savedState) {
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Inbox");
 
     }
 }
